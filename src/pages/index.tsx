c9953d69e45becc2ac43fcf0457e05934b3f89bd
Hlyan Htet <zawhlyan@gmail.com>
2023-03-10 06:35:12 +0630

    feat: modify index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,50 @@
+import EmailSearchInput from '@/components/EmailSearchInput';
+import Loader from '@/components/Loader';
 import ProfileMenu from '@/components/ProfileMenu';
 import type { NextPage } from 'next';
 import { getSession, useSession } from "next-auth/react";
+import { useState } from 'react';
 
 const Home: NextPage = () => {
-  const { data: session } = useSession();
+  const sessionData = useSession();
+  const { data: session } = sessionData;
+
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const searchMail = async (search: string) => {
+    setLoading(true);
+
+    const res = await fetch('/api/mail/search');
+
+    const data = await res.json();
+
+    console.log('searchMail', data);
+
+    setLoading(false);
+  };
 
   return (
-    <div className='px-10'>
+    <div className='px-10 text-gray-800 bg-slate-50 h-screen'>
       <div className='flex justify-end py-4'>
         <ProfileMenu />
       </div>
 
-      <h2 className='text-lg mt-10'>
-        Welcome, <span className='font-bold'>{session?.user?.name}</span>
-      </h2>
+
+      <div className='space-y-8 mt-20'>
+        <h2 className='text-lg'>
+          Welcome, <span className='font-bold'>{session?.user?.name}</span>
+        </h2>
+
+        {loading && <Loader />}
+
+        {!loading && <EmailSearchInput onSearch={searchMail} />}
+      </div>
     </div>
   )
 }
 
 export async function getServerSideProps(context: any) {
-  const session = await getSession(context)
+  const session = await getSession(context);
 
   if (!session) {
     return {
